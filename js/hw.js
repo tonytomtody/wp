9b4d363cfe9dd6f959a8a00cc0339b40dc615fa7
tonytomtody <tonytomtody@gmail.com>
2024-03-15 11:52:34 +0800

    new

--- js/hw.js
+++ js/hw.js
@@ -11,11 +11,15 @@ function printodd(n){
     }
 }
 function sumPrime(n){
-    let sum = 0
-    for (let i = 2 ; i < n ; i ++){
-        if (n % i != 0) sum = sum + n
+    let sum = 3
+    for (let i = 3 ; i <= n ; i ++){
+        for (let t = 2 ; i < n ; i ++){
+            if (i % t != 0) sum = sum + n
+        }
     }
+    return sum
 }
 console.log("2.")
 console.log(printodd(10))
 console.log("3.isPrime(5) = ",isPrime(5))
+console.log("4.sumPrime(5) = ",sumPrime(5))
