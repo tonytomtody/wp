d6470ad54e4751136ecb1ce4f844c38b0eec2a54
tonytomtody <tonytomtody@gmail.com>
2024-04-19 11:52:01 +0800

    first

---
+++ sha256/sha.js
@@ -0,0 +1,14 @@
+async function digestMessage(message) {
+  const msgUint8 = new TextEncoder().encode(message); // encode as (utf-8) Uint8Array
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8); // hash the message
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // convert buffer to byte array
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join(""); // convert bytes to hex string
+  return hashHex;
+}
+
+let s = document.querySelector('#textsrc');
+let r = document.querySelector('#res');
+console.log(s);
+digestMessage(s.value).then((digestHex) => r.innerHTML = digestHex);
