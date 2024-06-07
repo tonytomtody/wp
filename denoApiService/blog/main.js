018fbf29b28fdd05623e3d2b8a30dd418a37d231
tonytomtody <tonytomtody@gmail.com>
2024-06-07 09:41:13 +0800

    homework

--- denoApiService/blog/main.js
+++ denoApiService/blog/main.js
@@ -8,11 +8,11 @@ window.onhashchange = async function () {
   var tokens = window.location.hash.split('/')
   console.log('tokens=', tokens)
   switch (tokens[0]) {
-    case '#show':
+    case '#show': //show a post which is selected
       let r = await sqlFetch('blog', `SELECT id, title, body FROM posts WHERE id=${tokens[1]}`)
       R.show(r[0]) // 取得第一筆傳入 (雖然只會有一筆，但 SELECT 預設會傳回很多比，所以用 results[0] 只取第一筆)
       break
-    case '#new':
+    case '#new': //create a new post
       R.new()
       break
     default:
@@ -22,17 +22,17 @@ window.onhashchange = async function () {
   }
 }
 
-window.onload = async function () {
+window.onload = async function () { //when the window is loading,run the sqlFetch function
   await sqlFetch('blog', `CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, body TEXT)`)
   window.onhashchange()
 }
 
-R.layout = function (title, content) {
-  document.querySelector('title').innerText = title
-  document.querySelector('#content').innerHTML = content
-}
+R.layout = function (title, content) { //layout the title and content
+  document.querySelector('title').innerText = title //set the title
+  document.querySelector('#content').innerHTML = content //set the content
+} 
 
-R.list = function (posts) {
+R.list = function (posts) { //list all posts
   let list = []
   for (let post of posts) {
     list.push(`
@@ -53,7 +53,7 @@ R.list = function (posts) {
   return R.layout('Posts', content)
 }
 
-R.new = function () {
+R.new = function () { //create a new post's layout
   R.layout('New Post', `
   <h1>New Post</h1>
   <p>Create a new post.</p>
@@ -63,17 +63,17 @@ R.new = function () {
     <p><input id="savePost" type="button" value="Create"></p>
   </form>
   `)
-  document.querySelector('#savePost').onclick = ()=>R.savePost()
+  document.querySelector('#savePost').onclick = ()=>R.savePost() //save the post
 }
 
-R.show = function (post) {
+R.show = function (post) { //show the post selected
   return R.layout(post[_title], `
     <h1>${post[_title]}</h1>
     <p>${post[_body]}</p>
   `)
 }
 
-R.savePost = async function () {
+R.savePost = async function () { //save the post to the database
   let title = document.querySelector('#title').value
   let body = document.querySelector('#body').value
   await sqlFetch('blog', `INSERT INTO posts (title, body) VALUES ('${title}', '${body}')`)
